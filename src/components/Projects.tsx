0e77fd6a0e9ec3e7870059b9d55ea8a87735740c
odevpablo <odevpablo@gmail.com>
2025-06-27 16:47:22 -0300

    Update - Adição de mais projetos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import FabyModas from "../assets/fabymodas.jpg";
 import JoLima from "../assets/JoLima.jpg";
+import VDhair from "../assets/VdHair.jpg"
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import feedbackJoLima from "../assets/feedbackjolima.jpg";
@@ -10,10 +11,14 @@ import ResultadosJoLima2 from "../assets/ResultadorJoLima2.jpg";
 import VideosJoLima from "../assets/VideoJoLima.mp4";
 import TrabalhoJoLima from "../assets/TrabalhoJoLima2.jpg";
 import TrabalhoJoLima2 from "../assets/TrabalhoJoLima.jpg"
-import ResultadosFabyModas1 from "../assets/FabymodasResultados.jpg";
-import ResultadosFabyModas2 from "../assets/IMG-20250615-WA0032.jpg";
-import VideoFabyModas from "../assets/FABI MODA VESTUÁRIO .mov";
+import AntesFaby from "../assets/AlcanceFabymodasAntes.jpg";
+import DepoisFaby from "../assets/AlcanceFabyModasDepois.jpg";
+import VideoFabyModas from "../assets/VideoFabyModas.mp4"
+import VideoFabyModas2 from "../assets/FABI MODA VESTUÁRIO .mov";
 import TrabalhoFabyModas from "../assets/MetodologiasFabymodas.jpg"; 
+import VideoVdHair from "../assets/VideoVDHair.mp4"
+import VdHair from "../assets/VdHair2.jpg";
+import TemplateVdHair from "../assets/ImagemVDHair.jpg"
 import InstagramIcon from "../assets/instagram.png"; 
 import { useState } from "react";
 
@@ -40,10 +45,12 @@ function FabyModasModal({ open, onClose, project }) {
         <div>
           <p className="font-bold mb-2 mt-6">Meu trabalho</p>
           <video src={VideoFabyModas}  className="w-full h-auto object-cover rounded mb-4" controls/>
+          <video src={VideoFabyModas2}  className="w-full h-auto object-cover rounded mb-4" controls/>
           <img src={TrabalhoFabyModas}/>
-          <p className="font-bold mb-2 mt-6">Resultados alcançados</p>
-          <img src={ResultadosFabyModas2} className="w-full h-auto object-cover rounded mb-4" />
-          <img src={ResultadosFabyModas1}/>
+          <p className="font-bold mb-2 mt-6">Antes</p>
+          <img src={AntesFaby}/>
+          <p className="font-bold mb-2 mt-6">Depois</p>
+          <img src={DepoisFaby}/>
         </div>
       </div>
     </div>
@@ -89,6 +96,37 @@ function JoLimaModal({ open, onClose, project }) {
     </div>
   );
 }
+function VdHairModal({ open, onClose, project }) {
+  if (!open || !project) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-[#faf5f1] p-6 rounded-lg max-w-md w-full h-full overflow-y-auto">
+        <h3 className="text-2xl font-bold mb-4">{project.title}
+          <button
+            className="absolute top-6 text-2xl right-10 text-gray-500 hover:text-gray-800 focus:outline-none"
+            onClick={onClose}
+            aria-label="Fechar"
+          >
+            X
+          </button>
+          <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-600 underline ml-2">
+            <img src={InstagramIcon} alt="ig" className="inline w-6 h-6 align-middle hover:opacity-80" />
+          </a>
+        </h3>
+        <img src={project.image} alt={project.title} className="mb-4 rounded w-full" />
+        <p className="mb-4">{project.description}</p>
+        <div>
+          <p className="font-bold mb-2">Trabalho com metodologias escaláveis</p>
+          <video src={VideoVdHair} className="w-full h-auto object-cover rounded mb-4" controls/>
+          <p className="font-bold mb-2 mt-6">Construímos sua imagem digital com estratégia e autenticidade.</p>
+          <img src={TemplateVdHair}/>
+
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export const Projects = () => {
   const projects = [
@@ -105,14 +143,24 @@ export const Projects = () => {
       title: "Jo Lima Hair Teraphy",
       description: "Salão de beleza que oferece serviços voltados para cuidados pessoais e estéticos. Geralmente, salões como esse atuam com uma ampla gama de serviços.",
       image: JoLima,
-      tags: ["Comunicação", "Moda", "Atendimento"],
+      tags: ["Bem Estar", "Terapia", "Atendimento"],
+      link: "https://www.instagram.com/jolima_hairteraphy?utm_source=ig_web_button_share_sheet&igsh=MW1tcnlqdmQwa2I5YQ=="
+    },
+    {
+      id: "vd-Hair",
+      title: "VD Hair",
+      description: "VD Hair é um salão de beleza especializado em terapias capilares integrativas.",
+      image: VDhair,
+      tags: ["Bem Estar", "Terapia", "Atendimento"],
       link: "https://www.instagram.com/jolima_hairteraphy?utm_source=ig_web_button_share_sheet&igsh=MW1tcnlqdmQwa2I5YQ=="
     }
+    
   ];
 
   const [openModal, setOpenModal] = useState({
     "faby-modas": false,
-    "jo-lima": false
+    "jo-lima": false,
+    "vd-Hair": false
   });
   const [selectedProject, setSelectedProject] = useState(null);
 
@@ -155,7 +203,7 @@ export const Projects = () => {
                     ))}
                   </div>
                   <Button 
-                    className="w-full bg-blue-600 hover:bg-blue-700 text-white"
+                    className="w-full bg-[#6e7e85] hover:bg-blue-700 text-white"
                     onClick={() => showProject(project)}
                   >
                     Ver Projeto
@@ -167,7 +215,6 @@ export const Projects = () => {
         </div>
       </div>
 
-      {/* Modais */}
       <FabyModasModal 
         open={openModal["faby-modas"]} 
         onClose={() => closeModal("faby-modas")} 
@@ -179,6 +226,11 @@ export const Projects = () => {
         onClose={() => closeModal("jo-lima")} 
         project={selectedProject?.id === "jo-lima" ? selectedProject : null} 
       />
+      <VdHairModal
+      open={openModal["vd-Hair"]}
+      onClose={() => closeModal("vd-Hair")}
+      project={selectedProject?.id === "vd-Hair" ? selectedProject : null}
+    />
     </section>
   );
 };
